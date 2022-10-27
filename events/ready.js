5f2da92489ec6307a552df191371fbdfdd633aca
Remi4001 <remilabelle@outlook.com>
2022-10-27 17:01:50 -0400

    TODO for later

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,10 @@
+// const { interval } = require('./config.json');
+
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        // TODO: updateBot(); setInterval(updateBot, interval);
     },
 };
