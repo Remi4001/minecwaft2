13f950ba67189db34378c3e0553b10bfd1ee937f
Remi4001 <remilabelle@outlook.com>
2022-12-16 03:44:54 -0500

    Example running on linux

---
+++ launch_scripts/example2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Hello, world!"
+
+# example using the minecraft-server package on Arch Linux
+exec minecraftd start
+
+# example using tmux and a preconfigured script
+exec tmux new-session -dP -s mcserver /path/to/server/start.sh
