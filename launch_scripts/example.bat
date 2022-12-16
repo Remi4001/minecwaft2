9e9d4d7fc6378a23d843dda5d6c3bf16d3baa1ce
Remi4001 <remilabelle@outlook.com>
2022-12-16 02:44:18 -0500

    Moredetails

--- launch_scripts/example.bat
+++ launch_scripts/example.bat
@@ -1,2 +1,4 @@
-echo "Hello, world!"
-pause
+ECHO "Hello, world!"
+REM The next line opens a new command prompt and runs the specified script
+START /D path_to_server_folder CMD /c path_to_server_launch_script
+PAUSE
