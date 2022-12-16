79f8afadedb0ec4253ace041da70e45a0c5586a2
Remi4001 <remilabelle@outlook.com>
2022-12-16 14:08:40 -0500

    Add CHCP and call

--- launch_scripts/example.bat
+++ launch_scripts/example.bat
@@ -1,4 +1,7 @@
 ECHO "Hello, world!"
+
+REM Uncomment next line to support utf-8 characters
+REM CHCP 65001
+
 REM The next line opens a new command prompt and runs the specified script
-REM START /D path_to_server_folder CMD /c path_to_server_launch_script
-PAUSE
+REM START /D path_to_server_folder CMD /C call path_to_server_launch_script
