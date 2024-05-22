12d6238fcc5d1ad7d2bb7f3eb9f95af9d01b4d9b
Rémi Labelle <remi-2.labelle@polymtl.ca>
2024-05-22 17:59:26 -0400

    fix: exit script if cd fails

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || return
 
-while true
-	do
-		node .
-		echo Restarting in 10 seconds...
-		sleep 10
+while true; do
+	node .
+	echo Restarting in 10 seconds...
+	sleep 10
 done
