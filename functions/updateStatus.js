b8860d1615555ef1dbfb3d243d78c5829740ab1f
Rémi Labelle <remi-2.labelle@polymtl.ca>
2024-05-22 22:19:14 -0400

    fix: resolve some issues regarding types

--- functions/updateStatus.js
+++ functions/updateStatus.js
@@ -2,7 +2,7 @@
  * Updates the status of the discord bot
  * @param {import('discord.js').Client} client Discord client
  * @param {string} activity New activity name
- * @param {import('discord.js').PresenceStatus} status Discord status
+ * @param {import('discord.js').PresenceStatusData} status Discord status
  * @param {boolean} force Force the update
  */
 module.exports = function updateStatus(
@@ -12,12 +12,7 @@ module.exports = function updateStatus(
     force = false) {
     // If no current status
     if (!client.user.presence.activities[0] || force) {
-        client.user.setPresence({
-            activities: [{ name: activity }],
-            status: status,
-        });
-        console.log(`OldStatus: ${null}\tNewStatus: ${activity}, ${status}`);
-        return;
+        setStatus(client, activity, status);
     }
 
     const cActivity = client.user.presence.activities[0].name;
@@ -28,11 +23,19 @@ module.exports = function updateStatus(
 
     // if current status is different
     if (diffActivity || diffStatus) {
-        client.user.setPresence({
-            activities: [{ name: activity }],
-            status: status,
-        });
-        console.log(`OldStatus: ${cActivity}, ${cStatus}\t` +
-            `NewStatus: ${activity}, ${status}`);
+        setStatus(client, activity, status);
     }
 };
+
+/**
+ * @param {import('discord.js').Client} client
+ * @param {string} activity
+ * @param {import('discord.js').PresenceStatusData} status
+ */
+function setStatus(client, activity, status) {
+    client.user.setPresence({
+        activities: [{ name: activity }],
+        status,
+    });
+    console.log(`OldStatus: ${null}\tNewStatus: ${activity}, ${status}`);
+}
