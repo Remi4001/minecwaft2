2c217f037c78bf735c24462a37290260071b0f93
Rémi Labelle <remi-2.labelle@polymtl.ca>
2025-04-25 23:45:43 -0400

    fix: reapply status after being disconnected

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,8 @@
-const { updateBot } = require('../functions/updateBot.js');
-const { interval } = require('../../config.json');
+const { updateBot, parseStatus } = require('../functions/updateBot.js');
+const { interval, server } = require('../../config.json');
+const { type, ip, port } = server;
+const mcHermes = require('mc-hermes');
+const updateStatus = require('../functions/updateStatus.js');
 
 module.exports = {
     name: 'ready',
@@ -11,5 +14,12 @@ module.exports = {
         updateBot(client);
         console.log(`Ready! Logged in as ${client.user.tag}`);
         this.interval = setInterval(updateBot, interval, client);
+
+        // update status on reconnection
+        client.on('shardReady', () => {
+            mcHermes({ type, server: ip, port })
+                .catch(console.error)
+                .then(data => updateStatus(client, ...parseStatus(data), true));
+        });
     },
 };
